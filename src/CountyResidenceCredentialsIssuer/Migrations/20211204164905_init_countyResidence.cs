f449017aeb6715b38503f32cb993c5c6fa144826
damienbod <damien_bod@hotmail.com>
2021-12-04 17:49:36 +0100

    migrations

---
+++ src/CountyResidenceCredentialsIssuer/Migrations/20211204164905_init_countyResidence.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CountyResidenceCredentialsIssuer.Migrations
+{
+    public partial class init_countyResidence : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "CountyResidenceDataCredentials",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    OidcIssuerId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    OidcIssuer = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Did = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CountyResidenceDataCredentials", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CountyResidenceDataCredentials");
+        }
+    }
+}
